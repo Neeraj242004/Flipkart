a946b0e5d4ebbb6f11186bf990fa91a53461650c
Neeraj242004 <Neerajbhardwaj45d@gmail.con>
2025-11-27 23:14:09 +0530

    Fix NotFound import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './component/Navbar'
+import Navbar from './omponent/Navbar'
 import { Outlet } from 'react-router-dom'
 import MyStore from './Store/MyStore'
 import ModalForm from './AdminPanel/ModalForm'
