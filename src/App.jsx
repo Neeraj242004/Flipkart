914e2589e0a4e0a5234f6498312d75831e46b946
Neeraj242004 <Neerajbhardwaj45d@gmail.con>
2025-11-27 23:50:25 +0530

    Fix Navbar case-sensitive import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './component/Navbar'
+import Navbar from './Component/Navbar'
 import { Outlet } from 'react-router-dom'
 import MyStore from './Store/MyStore'
 import ModalForm from './AdminPanel/ModalForm'
